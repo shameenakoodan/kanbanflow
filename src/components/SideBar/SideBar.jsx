1f5f8d3e2c43dc04530b31ad270f2d1b064f3087
Shameena Koodan <shameena.koodan@gmail.com>
2023-09-26 12:33:59 -0500

    Added a button for the side bar for creating new boards

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import "./SideBar.scss";
 const SideBar = () => {
+    const board_icon = process.env.PUBLIC_URL + "/icons/board.png";
   return (
     <div className='sidebar'>
-        SideBar
+        <div>
+            ALL BOARDS(8)
+        </div>
+        <div className='new-board-button' >
+            <img src={board_icon} alt='board'/>
+            <input type='submit'  className='create-new-board' value={"+  Create New Board"}/>
+        </div>
     </div>
   )
 }
